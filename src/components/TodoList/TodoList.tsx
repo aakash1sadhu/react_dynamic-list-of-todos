da9a261c9be909dc7742483e63bbb0dcd7e4b088
Aakash Sadhu <aakashsadhu@hotmail.com>
2026-03-12 22:01:41 +0000

    fix

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 
 type Props = {
@@ -25,7 +26,7 @@ export const TodoList: React.FC<Props> = ({ todos, onSelect, selectedTodoId }) =
     <tbody>
       {todos.map(todo => (
 
-        <tr data-cy="todo" className="" key={todo.id}>
+        <tr data-cy="todo" className={classNames({ 'has-background-info-light': selectedTodoId === todo.id })} key={todo.id}>
         <td className="is-vcentered">{todo.id}</td>
         <td className="is-vcentered">
         {todo.completed && (
@@ -35,7 +36,9 @@ export const TodoList: React.FC<Props> = ({ todos, onSelect, selectedTodoId }) =
         )}
         </td>
         <td className="is-vcentered is-expanded">
-          <p className={todo.completed ? 'has-text-success' : 'has-text-danger'}>{todo.title}</p>
+          <p className={classNames({ 'has-text-success': todo.completed, 'has-text-danger': !todo.completed })}>
+            {todo.title}
+          </p>
         </td>
         <td className="has-text-right is-vcentered">
           <button data-cy="selectButton" className="button" type="button" onClick={() => onSelect(todo)}>
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export const TodoModal: React.FC<Props> = ({ todo, handleCloseModal }) => {
   const [user, setUser] = useState<User | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
